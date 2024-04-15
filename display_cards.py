15cba434269c747e62ea80f63340ad1006353b40
Alessandro Folloni <allefollo@gmail.com>
2024-04-15 10:13:06 +0200

    new feat

--- display_cards.py
+++ display_cards.py
@@ -1,28 +1,17 @@
-from PIL import Image
-import os
+from tkinter import *
+from tkinter import ttk
 
-def display_cards(card_paths):
-    images = []
-    for path in card_paths:
-        images.append(Image.open(path))
+def oscura():
+    pass
 
-    # Calcola le dimensioni totali per la griglia
-    max_width = max(image.width for image in images)
-    total_height = sum(image.height for image in images)
 
-    # Crea un'immagine vuota per disegnare le carte
-    result = Image.new("RGB", (max_width, total_height), color=(255, 255, 255))
+root = Tk()
+root.title("BLACKJACK TABLE")
+root.geometry("600x400")
+icon = PhotoImage(file="images/utils/logo.png")
+root.iconphoto(True, icon)
 
-    # Disegna le carte sulla nuova immagine
-    y_offset = 0
-    for image in images:
-        result.paste(image, (0, y_offset))
-        y_offset += image.height
+button = Button(command=oscura)
+button.pack()
 
-    # Mostra l'immagine
-    result.show()
-
-if __name__ == "__main__":
-    # Assicurati di avere le immagini delle carte da poker nella stessa directory del tuo script
-    card_paths = [f for f in os.listdir() if f.endswith('.jpg')]  # Modifica il filtro in base al formato delle tue immagini
-    display_cards(card_paths)
+root.mainloop()
--- main.py
+++ main.py
@@ -12,6 +12,16 @@ def num_mazzi():
                 print("Errore! Inserisci un numero valido.")
     return numero
 
+def num_players():
+    while True:
+        num_players = input("Inserisci numero di giocatori in gioco: ")
+        if not num_players.isdigit():
+            continue
+        else:
+            break
+
+    return num_players
+
 def crea_mazzo(num_mazzi):
     mazzo = {}
     valori = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Regina', 'Re', 'Asso']
@@ -19,12 +29,83 @@ def crea_mazzo(num_mazzi):
         mazzo[valore] = 4 * num_mazzi
     return mazzo
 
+def admitted(card):
+    if card == "Asso" or card == "Jack" or card == "Re" or card == "Regina":
+        return True
+
+    if card.isdigit():
+        num = float(card)
+        if num >= 2 or num <= 10:
+            return True
+
+    return False
+
+def your_cards():
+    input_1 = input("Inserisci le tue carte: ")
+    input_2 = input("")
+    while True:
+        if admitted(input_1):
+            print("Valore 1 ok")
+            if admitted(input_2):
+                print("Valore 2 ok")
+                break
+            else:
+                print("Valore 2 errato, riprova")
+                input_2 = input("")
+        else:
+            print("Valore 1 errato, riprova")
+            input_1 = input("")
+            continue
+
+    return input_1, input_2
+
+def opponent_card():
+    while True:
+        input_d = input("Inserisci la carta del dealer: ")
+        if admitted(input_d):
+            print("Carta del dealer OK")
+            return input_d
+
+
+def carte_uscite(mazzo):
+    print("Aggiungi tutte le carte che escono")
+    while True:
+        carta = input("")
+        if not admitted(carta):
+            print("Not valid, try again")
+            a = input("Vuoi inserirne un altro? si/no")
+            if a !="si":
+                break
+        else:
+            key = carta
+            mazzo[key] -= 1
+            print("Rimosso un " + str(key))
+            a = input("Vuoi inserirne un altro? si/no: ")
+            if a != "si":
+                print()
+                break
+    return
+
+
 
 if __name__ == '__main__':
     num_mazzi = num_mazzi()
-    mazzo_scala = crea_mazzo(num_mazzi)
+    tot_carte = 52 * num_mazzi
+    print(tot_carte)
+    players = num_players()
+    mazzo = crea_mazzo(num_mazzi)
+    print(mazzo)
+    carta1, carta2 = your_cards()
+    dealer = opponent_card()
 
-    # Stampiamo il dizionario per verificare
-    for carta, numero in mazzo_scala.items():
-        print(f"{carta}: {numero}")
+    # aggiorna mazzo
+    mazzo[carta1] -= 1
+    mazzo[carta2] -= 1
+    mazzo[dealer] -= 1
+    tot_carte -= 3
+    print(tot_carte)
+
+    carte_uscite(mazzo)
+
+
 
